540469a40c6ea2807926f7847067357086a62a99
Віктор Павлюк <pavlyuvictor@gmail.com>
2026-08-18 15:36:04 +0300

    Fix Quality Gate

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'mydb'),
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', '12345'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD',),
         'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
         'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
